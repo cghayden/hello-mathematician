91406ab001a75e9bfecf380b0b05a6e035b195f9
Corey <cghayden@gmail.com>
2020-04-10 14:28:11 -0400

    animate only full equation in and out

--- src/components/Equation.js
+++ src/components/Equation.js
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect } from "react";
 import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
 
 // import Operand from "./Operand";
 
@@ -22,44 +23,53 @@ export default function Input({
   useEffect(() => {
     inputEl.current.focus();
     // labelRef.current.click();
-  });
+  }, [digits]);
 
   function handleInputChange(e) {
     setAnswer(e.target.value, 10);
   }
   return (
-    <FullEquation
-      className="fullEquation"
-      id="inputForm"
-      action="POST"
-      onSubmit={(e) => checkAnswer(e)}
-    >
-      <label htmlFor="answer">
-        <OperandContainer className="operandContainer">
-          {/* <GhostOperand>{digits[0]}</GhostOperand> */}
-          <p>{digits[0]}</p>
-        </OperandContainer>
-        <p>{view}</p>
-        <OperandContainer>
-          {/* <GhostOperand>{digits[1]}</GhostOperand> */}
-          <p>{digits[1]}</p>
-        </OperandContainer>
-        <p className="equals">=</p>
-        {isCorrect === false && (
-          <span className="revealCorrect">{solution}</span>
-        )}
-        <input
-          autoFocus
-          id="answer"
-          type="number"
-          pattern="[0-9]*"
-          ref={inputEl}
-          value={answer}
-          name="answer"
-          onChange={(e) => handleInputChange(e)}
-        />
-      </label>
-    </FullEquation>
+    <AnimatePresence exitBeforeEnter>
+      <motion.div
+        key={`${digits[0]},${digits[1]},${solution}}`}
+        initial={{ x: 300, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        exit={{ x: -300, opacity: 0 }}
+      >
+        <FullEquation
+          className="fullEquation"
+          id="inputForm"
+          action="POST"
+          onSubmit={(e) => checkAnswer(e)}
+        >
+          <label htmlFor="answer">
+            <OperandContainer className="operandContainer">
+              {/* <GhostOperand>{digits[0]}</GhostOperand> */}
+              <p>{digits[0]}</p>
+            </OperandContainer>
+            <p>{view}</p>
+            <OperandContainer>
+              {/* <GhostOperand>{digits[1]}</GhostOperand> */}
+              <p>{digits[1]}</p>
+            </OperandContainer>
+            <p className="equals">=</p>
+            {isCorrect === false && (
+              <span className="revealCorrect">{solution}</span>
+            )}
+            <input
+              autoFocus
+              id="answer"
+              type="number"
+              pattern="[0-9]*"
+              ref={inputEl}
+              value={answer}
+              name="answer"
+              onChange={(e) => handleInputChange(e)}
+            />
+          </label>
+        </FullEquation>
+      </motion.div>
+    </AnimatePresence>
   );
 }
 
